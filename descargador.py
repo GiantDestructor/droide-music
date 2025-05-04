fb4c508a91415e94d91e6f518fe04d0b2548b910
Giant <destructorbuck@gmail.com>
2025-05-04 17:05:46 -0600

    Ya descarga metadatos pero no pone imagen

--- descargador.py
+++ descargador.py
@@ -13,6 +13,7 @@ from kivy.uix.anchorlayout import AnchorLayout
 from kivy.uix.gridlayout import GridLayout
 import requests
 from mutagen.mp4 import MP4, MP4Cover
+from mutagen.m4a import M4A, M4ACover
 #from android.storage import primary_external_storage_path
 import os
 
@@ -72,42 +73,54 @@ class Aplicacion(GridLayout):
     URLS = "" 
     nombre_del_artista = StringProperty("")
     nombre_del_album = StringProperty("")
-    directorio = r"C:\Users\PC\Desktop\videos_descargados\Descargador_Musica_Android\Pruebas"
+    directorio = r""
+    info = ""
+    directorio_full= r""
+    #directorio_completo = r""
     
 
     pass
 
-    def descargar_audio(self, directorio):
+    def descargar_audio(self):
         self.ydl_opts = {
         'ignoreerrors': True,
-        'format': 'm4a/bestaudio/best', 'paths': {"home": directorio},
-        'postprocessors': 
-        [{  
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3'
-        },
-        
-        {
-        'key': 'FFmpegMetadata',
-        
-        },
-        {
-        'key': 'EmbedThumbnail'
-        }
-        ],
-        'postprocessor_args': [  
-        '-metadata', f'artist={self.nombre_del_artista}',
-        '-metadata', f'album={self.nombre_del_album}',
-        ],
-        "writethumbnail" : True
-            
+        'format': 'm4a/bestaudio/best', 'paths': {"home": self.directorio},
         }
         return self.ydl_opts
     
     def descargar_boton(self):
-        self.descargar_audio(self.directorio)
+        
+        #Se usa el directorio y si no existe, crea el directorio
+       # primary_external_storage_path()
+        self.directorio= os.path.join(os.getcwd(), "Music")
+        if not os.path.exists(self.directorio):
+            os.makedirs(self.directorio)
+        
+
+        """try:
+            audio = MP4(self.directorio)
+        except PermissionError: ("cagaste")"""
+        
+        self.descargar_audio()
         with yt_dlp.YoutubeDL(self.ydl_opts) as ydl:
-            ydl.download(self.URLS)  #Definir ydl opts que son las opciones
+            self.info = ydl.extract_info(self.URLS, download=True)
+            self.directorio_full= os.path.join(self.directorio, ydl.prepare_filename(self.info).replace(".webm", ".m4a").replace(".m4a.m4a", ".m4a"))
+            self.agregar_metadatos()
+
+    def agregar_metadatos(self):
+        audio = MP4(self.directorio_full)
+        audio.tags["\xa9ART"] = self.nombre_del_artista  # Artista
+        audio.tags["\xa9alb"] = self.nombre_del_album  # Álbum
+
+        
+        portada_url = self.info.get("thumbnail", None)
+        if portada_url:
+            imagen = requests.get(portada_url).content
+            audio["covr"] = [MP4Cover(imagen, imageformat=MP4Cover.FORMAT_JPEG)]
+        
+        audio.save()
+
+        
 class Descargador(App):
     def build(self):
         return Aplicacion()
